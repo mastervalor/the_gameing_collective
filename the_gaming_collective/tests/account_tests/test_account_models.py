6abdc42ea46cec5248ff9ff71adf40f6ede6be37
mastervalor <mouradmarzouk63@gmail.com>
2024-09-19 23:43:55 -0700

    Update test_account_models.py

--- the_gaming_collective/tests/account_tests/test_account_models.py
+++ the_gaming_collective/tests/account_tests/test_account_models.py
@@ -16,3 +16,16 @@ class TestAccountModelsUserManager(TestCase):
         
         errors = self.user_manager.default_user_validator(postData)
         self.assertEqual(errors, {})
+        
+    def test_invalid_email(self):
+        postData = {
+            'email': 'invalid.email',
+            'password': 'ValidPassword1!',
+            'password_confirm': 'ValidPassword1!',
+            'first_name': 'John',
+            'last_name': 'Doe'
+        }
+        
+        errors = self.user_manager.default_user_validator(postData)
+        self.assertIn('login', errors)
+        
