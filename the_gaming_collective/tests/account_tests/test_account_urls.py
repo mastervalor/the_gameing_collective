e454cfdef9659b2abbb92c0d2a3e1db1d8492af1
mastervalor <mouradmarzouk63@gmail.com>
2024-10-30 19:06:07 -0700

    Update test_account_urls.py

--- the_gaming_collective/tests/account_tests/test_account_urls.py
+++ the_gaming_collective/tests/account_tests/test_account_urls.py
@@ -5,4 +5,7 @@ from account.views import (
     login_view, edit_account, update_account, delete_account, logout_view
 )
 
-
+class TestUrls(SimpleTestCase):
+    def test_login_create_url_resolves(self):
+        url = '/login_create/'
+        self.assertEqual(resolve(url).func, index)
